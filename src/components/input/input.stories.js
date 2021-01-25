f111968b28faf396b7a166e3927d3743b383fee3
fgottes <f.gottesmann@outlook.com>
2021-01-25 16:48:33 +0100

    create inputbox

---
+++ src/components/input/input.stories.js
@@ -0,0 +1,11 @@
+import "./input.css";
+import inputbox from "./input.html";
+
+
+export default {
+  title: "Components/input",
+  parameters: { layout: "centered" },
+};
+
+
+export const border = () => inputbox;
