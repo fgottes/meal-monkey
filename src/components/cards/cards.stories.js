ff52154f314ed33e22705bed5d1b0fb698af36b4
fgottes <f.gottesmann@outlook.com>
2021-01-27 15:12:23 +0100

    grid container layout

--- src/components/cards/cards.stories.js
+++ src/components/cards/cards.stories.js
@@ -2,6 +2,8 @@ import "./cards.css";
 import cardAfrican from "./card-african.html";
 import cardAmerican from "./card-american.html";
 import cardSpanish from "./card-spanish.html";
+import cardContainer from "./card-container.html";
+
 
 export default {
   title: "Components/Cards",
@@ -11,3 +13,4 @@ export default {
 export const african = () => cardAfrican;
 export const american = () => cardAmerican;
 export const spanish = () => cardSpanish;
+export const multiple = () => cardContainer;
